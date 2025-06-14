63bf025fa8faddb72e0e7d6df1558130a2ca71cf
Kenzen92 <jamespeterkenny@googlemail.com>
2025-06-14 22:21:01 +0100

    update docker commands

--- install_sensor.sh
+++ install_sensor.sh
@@ -5,7 +5,7 @@ set -e  # Exit immediately if any command fails
 VENV_DIR="python/venv"
 
 echo "Stopping any running containers..."
-docker-compose down || true
+docker compose down || true
 
 echo "Setting up Python virtual environment..."
 # Check if venv exists, create it if it doesn't
@@ -32,12 +32,12 @@ npm run build
 cd ../
 
 echo "Building Docker containers..."
-docker-compose build
+docker compose build
 
 echo "Starting Docker containers in detached mode..."
-docker-compose up -d
+docker compose up -d
 
-# Ensure docker-compose starts successfully before proceeding
+# Ensure docker compose starts successfully before proceeding
 if [ $? -eq 0 ]; then
   echo "Docker containers started successfully."
 else
